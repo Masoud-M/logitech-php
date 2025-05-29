15e9e09fdd9052bd87d6e0a93b08fc2fc05a45bc
Masoud-M <48066903+Masoud-M@users.noreply.github.com>
2025-05-29 11:09:59 +0330

    chore: removed unnecessary comment

--- src/router.php
+++ src/router.php
@@ -1,19 +1,11 @@
 <?php
 
-// $productsJson = file_get_contents("models/products.json");
-// $productsData = json_decode($productsJson, true);
-// foreach ($productsData as $productData) {
-//     $slug_titles[] = ($productData['slug_title']);
-// }
-
-// If it was index page, show the index template
-// If it has slug title after "products", grab the id from prams and match it to ids from products.json and show the product template with the given id
-// if it wasn't either of them show the 404 template
-
 $routes = [
     "/" => "controllers/index.php",
     "/product" => "controllers/product.php",
 ];
+
+
 
 if (array_key_exists(urlPathname(), $routes)) {
     require $routes[urlPathname()];
